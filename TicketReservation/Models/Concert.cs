2bb39287378bd68bd629121f0ed18a96c6b45293
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2021-08-08 15:08:11 +0200

    Added Concert.cs class implementation

--- TicketReservation/Models/Concert.cs
+++ TicketReservation/Models/Concert.cs
@@ -8,5 +8,9 @@ namespace TicketReservation.Models
     public class Concert
     {
         public Artist ArtistObj { get; set; }
+        public DateTime Date { get; set; }
+        public Address ConcertAddress { get; set; }
+        public string Description { get; set; }
+        public int NumberOfTickets { get; set; }
     }
 }
