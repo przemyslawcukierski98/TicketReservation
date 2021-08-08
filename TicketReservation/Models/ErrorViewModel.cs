d09332adf2b6df0796ffa15bd72dfdd047a7788e
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2021-08-08 12:36:13 +0200

    Initial commit

---
+++ TicketReservation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TicketReservation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
