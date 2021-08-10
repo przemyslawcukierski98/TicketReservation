4a7c07b40a19d9ce7e30f374eebdfc3fa582e7f8
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2021-08-10 15:50:59 +0200

    Passing data from the ConcertsController to the view

--- TicketReservation/Controllers/ConcertsController.cs
+++ TicketReservation/Controllers/ConcertsController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TicketReservation.Models;
 
 namespace TicketReservation.Controllers
 {
@@ -10,6 +11,17 @@ namespace TicketReservation.Controllers
     {
         public IActionResult ViewAllConcerts()
         {
+            List<ConcertDetails> Details = new List<ConcertDetails>();
+
+            ConcertDetails CartiDetails = new ConcertDetails("Playboy Carti w Hali Torwar", "od 149.99 PLN", $"Jedyny taki występ w Polsce! Playboy Carti zaprezentuje po raz pierwszy na żywo utwory z wersji Deluxe kultowego albumu \"Whole Lotta Red\"!", "https://is5-ssl.mzstatic.com/image/thumb/Features115/v4/4f/6c/cf/4f6ccf13-9927-7965-d954-3178a49f8e0c/mzl.iquhwvbg.jpg/375x375cc.webp");
+            ConcertDetails CoolDetails = new ConcertDetails("COOL FESTIVAL - Hala Stulecia, Wrocław", "od 299.99 PLN", "Arca, Bjork, Tyler the Creator, Amnesia Scanner, IC3PEAK, tylko w Hali Stulecia", "https://muno.pl/wp-content/uploads/2020/05/Arca-850x570.jpg");
+            ConcertDetails ZenekDetails = new ConcertDetails("Zenek Martyniuk Symfonicznie", "od 99.99 PLN", "Największe hity króla disco polo w wykonaniu Narodowej Orkiestry Symfonicznej Polskiego Radia. Jedyne takie wydarzenie w stolicy Podkarpacia!", "https://i.iplsc.com/zenek-martyniuk/0009PL2T6I1M5SJB-C122-F4.jpg");
+
+            Details.Add(CartiDetails);
+            Details.Add(CoolDetails);
+            Details.Add(ZenekDetails);
+
+            ViewBag.details = Details;
             return View();
         }
 
---
+++ TicketReservation/Models/ConcertDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketReservation.Models
+{
+    public class ConcertDetails
+    {
+        public ConcertDetails(string concertTitle, string underTitle, string description, string imageUri)
+        {
+            ConcertTitle = concertTitle;
+            UnderTitle = underTitle;
+            Description = description;
+            ImageUri = imageUri;
+        }
+
+        public string ConcertTitle { get; set; }
+        public string UnderTitle { get; set; }
+        public string Description { get; set; }
+        public string ImageUri { get; set; }
+    }
+}
