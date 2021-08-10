a451435eabf87674b1bd84e0b434db76710a2388
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2021-08-10 22:06:10 +0200

    Added ArtistPom.cs, PageConcertObject.cs, TicketOption.cs classes

---
+++ TicketReservation/Models/PageConcertObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketReservation.Models
+{
+    public class PageConcertObject
+    {
+        public string Header { get; set; }
+        public string HeaderDescription { get; set; }
+        public string MainImageUri { get; set; }
+        public string ConcertDescription { get; set; }
+        public List<ArtistPom> ArtistPomList { get; set; }
+        public List<TicketOption> TicketOptions { get; set; }
+    }
+}
---
+++ TicketReservation/Models/TicketOption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketReservation.Models
+{
+    public class TicketOption
+    {
+        public string TicketHeader { get; set; }
+        public List<string> TicketDetails { get; set; }
+        public string TicketPrice { get; set; }
+    }
+}
