83ac5f7ae3225946ff74bcd2cf2cbced99c2a777
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2021-08-14 18:42:16 +0200

    Created basic layout for ordering ticket

---
+++ TicketReservation/Controllers/TicketsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketReservation.Controllers
+{
+    public class TicketsController : Controller
+    {
+        public IActionResult OrderingTicket()
+        {
+            return View();
+        }
+    }
+}
